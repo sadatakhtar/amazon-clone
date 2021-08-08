00111f4cbfa1f2bc490b7eaaa4bf8cb6d2742edd
sadat <sadat0akhtar@gmail.com>
2021-08-08 17:09:16 +0100

    initial commit, added firebase credentials

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDPivMdaJFTfMI8cJ4XNdbGNHjUv3nPsI",
+    authDomain: "clone-76120.firebaseapp.com",
+    projectId: "clone-76120",
+    storageBucket: "clone-76120.appspot.com",
+    messagingSenderId: "354383344870",
+    appId: "1:354383344870:web:67951166e9252b5aec7727",
+    measurementId: "G-WY3KR9E2Y4"
+  };
