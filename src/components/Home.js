9b19764651e35597de9b4d86c4a6ce5289d9ebb2
sadat <sadat0akhtar@gmail.com>
2021-08-11 00:21:39 +0100

    created products component and added fake products

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,18 +12,47 @@ function Home() {
           alt="banner"
         />
         <div className="home_row">
-          <Product />
-          <Product />
+          <Product 
+            title='The Lean Startup: How Todays Entrepreneurs use Continuous Innovation to create radically successful businesses' 
+            price={9.99} 
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={4}
+            />
+          <Product 
+            title='Muddyfox bike pump'
+            price={10.99}
+            image="https://images.sportsdirect.com/images/products/93780611_l.jpg"
+            rating={3}
+          />
         </div>
 
         <div className="home_row">
-          <Product />
-          <Product />
-          <Product />
+          <Product 
+            title='Cross Adults Bike Helmet' 
+            price={19.99} 
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGhC86e2Lk_4v91ebVXFAKB-GAlzxqFIR8Mcoy1Xh3NxRQRkIGpOCodmdDcjBG1lPbsohHLj4&usqp=CAc" 
+            rating={5}
+          />
+          <Product 
+            title='Cycling Jersey Mens Long Sleeves' 
+            price={15.88} 
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToCik_dWaHjVBgS8llldHKtOQfC2VH8yqNxD8gu3zvEGV2zByz1TJa-Z4ZHPnEnBcGxmuPxw0F&usqp=CAc" 
+            rating={3}/>
+          <Product 
+            title='Samsung Galaxy Book pro 360 15.6", i7, 16GB in Mystic Navy'
+            price={1,499.00}
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREYtMLgIXds4wpTAEAkXqnbrMI-HDR0-rzm8mh5_FQwrZ9tVtmbod0MHvtC4nHhOeLo9FeT99D&usqp=CAc"
+            rating={4}
+          />
         </div>
 
         <div className="home_row">
-          <Product />
+          <Product 
+            title='Amazon echo Plus 2. Generation sprachassistent black' 
+            price={167.42} 
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK29EHZxBPjmpZE_XqHhAhl18cpkLVjxd2Plu-6iqQoYIX5njtJXv9p5wr&usqp=CAc" 
+            rating={5}
+          />
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({title, price, image, rating}) {
     return (
         <div className='product'>
             <div className='product_info'>
-                <p>The Lean startup:</p>
+                <p>{title}</p>
                 <p className='product_price'>
                     <small>$</small>
-                    <strong>11.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className='product_rating'>
-                    <p>⭐</p>
-                    <p>⭐</p>
-                    <p>⭐</p>
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+
             
                 </div>
 
             </div>
-            <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' alt='product'/>
-            <button>Add to backet</button>
+            <img src={image}/>
+            <button>Add to basket</button>
 
             
             
