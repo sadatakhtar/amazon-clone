06886b92a0f577c20f609d962dbec3a0f4de4312
sadat <sadat0akhtar@gmail.com>
2021-08-14 23:27:11 +0100

    continued on payments component

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -8,7 +8,7 @@ function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
-      <div className="paymeny_container">
+      <div className="payment_container">
         <h1>
           Checkout (<Link to="/checkout">{basket?.length} items</Link>)
         </h1>
@@ -16,13 +16,12 @@ function Payment() {
         <div className="payment_section">
           <div className="payment_title">
             <h3>Delivery Address</h3>
-
-            <div className="payment_address">
+          </div>
+          <div className="payment_address">
               <p>{user?.email}</p>
               <p>123 React Lane</p>
               <p>Birmingham B1 1AB</p>
             </div>
-          </div>
         </div>
 
         {/* payment section - review items */}
